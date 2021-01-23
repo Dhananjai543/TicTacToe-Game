0e22fea0cb980832cc0dcac8da71b07461302967
Dhananjai <dhananjaisaini9@gmail.com>
2021-01-23 12:46:24 +0530

    Initial commit

---
+++ Player.java
@@ -0,0 +1,31 @@
+package ticTacToeGame;
+
+public class Player {
+	
+	private String name;
+	private char symbol;
+	
+	public Player(String name, char symbol){
+		setName(name);
+		setSymbol(symbol);
+	}
+
+	public void setSymbol(char symbol) {
+		if(symbol != '\0') {
+			this.symbol = symbol;
+		}
+		
+	}
+
+	private void setName(String name) {
+		if(!name.isEmpty()) {
+			this.name = name;
+		}
+	}
+	public String getName() {
+		return this.name;
+	}
+	public char getSymbol() {
+		return this.symbol;
+	}
+}
